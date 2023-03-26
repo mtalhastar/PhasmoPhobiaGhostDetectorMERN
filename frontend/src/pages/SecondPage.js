f0efe5136318f4fc0773fdbea652fae39bed8898
mtalhastar <mtalhastar@gmail.com>
2023-03-26 23:04:53 +0500

    commit

--- frontend/src/pages/SecondPage.js
+++ frontend/src/pages/SecondPage.js
@@ -9,29 +9,47 @@ const refresh=()=>{
 
 const SecondPage =()=>{
    const [evidences,setevidences] =useState(null)
-   const [ghosts,setghost]=useState(null)
+   const [ghost,setghost]=useState(null)
     const [matchingGhosts, setMatchingGhosts] = useState([]);
     const [selectedEvidences, setSelectedEvidences] = useState([]);
     const [selectedCards,setSelectedCards]=useState([])
-    const [game, setGame] = useState('')
-    const [gameList,setGameList] = useState([])
-    const [selectedOption, setSelectedOption] = useState('');
-
-    /*const handleSetGhosts = (matchingGhost) => {
+    const [gamelist,setgamelist]=useState([])
+    const[game,setgame]=useState('Phasmophobia')
+    const[extraghosts,setghostsextra]=useState([])
+/*const handleSetGhosts = (matchingGhost) => {
     setMatchingGhosts(matchingGhost);
-   };
-   */
-  
+};
+*/
 const handleCardSelect=(evidence)=>{
   const updatedSelectedCards = [...selectedCards, evidence];
     setSelectedCards(updatedSelectedCards);
-    const filteredGhosts = ghosts.filter((ghost) =>
+    const filteredGhosts = ghost.filter((ghost) =>
       updatedSelectedCards.every((selected) =>
-        ghost.EvidenceList.evidences.includes(selected.Name)
+        ghost.EvidenceList.includes(selected.Name)
       )
     );
     setMatchingGhosts(filteredGhosts);
 }
+
+
+ const startGame =()=>{
+  const ghooost = extraghosts
+  console.log(JSON.stringify(ghost))
+     const updatedGhosts = ghooost.map((ghost) => ({
+        ...ghost,
+        EvidenceList: ghost.EvidenceList.filter((evidenceList) => evidenceList.Game ===game )
+          .map((evidenceList) => evidenceList.evidences)
+          .flat(),
+      }));
+    setghost(updatedGhosts)
+    console.log(JSON.stringify(updatedGhosts))
+ }
+
+
+
+const handleOptionChange = (event) => {
+     setgame(event.target.value);
+     };
 
 
 const handleUnSelect=(evidence)=>{
@@ -43,18 +61,14 @@ const handleUnSelect=(evidence)=>{
     if (updatedSelectedCards.length === 0) {
       setMatchingGhosts([]);
     } else {
-      const filteredGhosts = ghosts.filter((ghost) =>
+      const filteredGhosts = ghost.filter((ghost) =>
         updatedSelectedCards.every((selected) =>
-          ghost.EvidenceList.evidences.includes(selected.Name)
+          ghost.EvidenceList.includes(selected.Name)
         )
       );
       setMatchingGhosts(filteredGhosts);
     }
 }
-
-const handleOptionChange = (event) => {
-     setSelectedOption(event.target.value);
-};
 
 const handleSetEvidence = (matchingEvidences, removeGhost) => {
   const uniqueEvidencesSet = new Set(selectedEvidences.concat(matchingEvidences));
@@ -67,40 +81,51 @@ const handleSetEvidence = (matchingEvidences, removeGhost) => {
   }
   setSelectedEvidences(uniqueEvidences);
 }
+const returnElement=()=>{
+ console.log(gamelist)
+ console.log(ghost)
+ console.log(evidences)
+}
+  
    useEffect(()=> {
-      
-         const fetchEvidence=async()=>{
+        
+    
+
+   const fetchEvidence=async()=>{
           
-         const evidence=await fetch('/evidence/')
-         const gamelisting=await fetch('/game')
+         const evidence=await fetch('/evidence')
          const ghost=await fetch('/ghost')
-        const gamejson = await gamelisting.json()
+          const games=await fetch('/game')
+          const gamejson   = await games.json()
          const ghostjson=await ghost.json()
          const json = await evidence.json()
-         if(game.ok){
-          setGameList([...gamejson])
-      
-         }
          if(evidence.ok){
            setevidences([...json])
+           setghost([...ghostjson])
+           setghostsextra([...ghostjson])
+           setgamelist([...gamejson])
          }
-          setghost([...ghostjson])
-        /* const updatedGhosts = ghosts.map((ghost) => ({
-         ...ghost,
-         EvidenceList: ghost.EvidenceList.filter((evidenceList) => evidenceList.Game === game)
-         .map((evidenceList) => evidenceList.evidences)
-         .flat()
-         }));
-         if(updatedGhosts){
-         
-          setghost([...ghostjson])
-         }*/
          }
          fetchEvidence()
-    },[ghosts,gameList])
+    },[])
 return(
         <main>
+
         <section className="evidence-container">
+
+            <select className="dropdown" 
+             
+              onChange={handleOptionChange}
+              >
+              <option value="">Select the Game</option>
+             {gamelist && gamelist.map((option) => (
+             <option   key={option._id}    value={option.Name}>
+             {option.Name}
+             </option>
+             ))}
+             </select>  
+           <button onClick={startGame}>startGame</button>
+
         <section className="evidence-section">
          <h2>Evidence</h2>
         {selectedEvidences && selectedEvidences.map((content) => (
@@ -118,26 +143,13 @@ return(
        <section className="ghost-evidence-section">
         <h2>Ghost Evidence</h2>
         <button onClick={refresh}>Reset</button>
-     
-         <select className="dropdown" 
-             value={selectedOption}
-             onChange={handleOptionChange}
-      
-        >
-         <option value=""></option>
-         {gameList && gameList.map((option) => (
-            <option   key={option._id}     value={option.Name}>
-            {option.Name}
-           </option>
-         ))}
-         </select>             
-           
         <div className="cards">
         {evidences&&evidences.map((element)=>(
-            <Card key={element._id} e= {element} ghosts={ghosts} handleCardSelect={handleCardSelect}  handleUnSelect={handleUnSelect} evidence={evidences} setEvidence={handleSetEvidence} selectedEvidence={selectedEvidences} setSelectedEvidence={setSelectedEvidences}  matchghost={matchingGhosts} />
+            <Card key={element._id} e= {element} ghosts={ghost} handleCardSelect={handleCardSelect}  handleUnSelect={handleUnSelect} evidence={evidences} setEvidence={handleSetEvidence} selectedEvidence={selectedEvidences} setSelectedEvidence={setSelectedEvidences}  matchghost={matchingGhosts} />
          ))
         }
       </div>
+         
        </section>
     </main>
 )
