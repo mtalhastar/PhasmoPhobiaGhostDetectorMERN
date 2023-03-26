111483e6e2822172cfc7f0353324b5f752444cfa
mtalhastar <mtalhastar@gmail.com>
2023-03-26 17:19:23 +0500

    commit

--- frontend/src/component/GhostForm.js
+++ frontend/src/component/GhostForm.js
@@ -1,10 +1,32 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 
 const GhostForm = () => {
     const [name, setName] = useState('')
     const [evidenceList, setEvidenceList] = useState('')
     const [error, setError] = useState(null)
     const [emptyFields, setEmptyFields] = useState([])
+    const [selectedOption, setSelectedOption] = useState('');
+    const [allevidenceList,setAllEvidenceList]=useState('')
+
+
+
+  useEffect(() => {
+        
+        const fetchEvidence = async () => {
+            const response = await fetch('/evidence')
+            const json = await response.json()
+
+            if (response.ok) {
+                setAllEvidenceList([...json])
+            }
+        }
+
+        fetchEvidence()
+    },[])
+
+
+
+
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -34,6 +56,18 @@ const GhostForm = () => {
         }
     }
 
+
+     const handleOptionChange = (event) => {
+     setSelectedOption(event.target.value);
+     };
+
+    const handleAddEvidence = () => {
+     if (selectedOption) {
+      setEvidenceList([...evidenceList, selectedOption]);
+      setSelectedOption('');
+     }
+     };
+
     return (
         <form className="create" onSubmit={handleSubmit}>
             <h3 className="h31">Add Ghost Information</h3>
@@ -43,6 +77,7 @@ const GhostForm = () => {
                 type="text" 
                 onChange={(e) => setName(e.target.value)}
                 value={name} 
+                required
                 className={emptyFields.includes('name') ? 'error' : ''}
             />
 
@@ -51,12 +86,14 @@ const GhostForm = () => {
             <label className="label1">You can assign multiple evidences here"</label>
              <select className="dropdown" required >
              <option value="">Select evidence</option>
-             <option value="evidence1">Evidence 1</option>
-             <option value="evidence2">Evidence 2</option>
-             <option value="evidence3">Evidence 3</option>
+             {allevidenceList && allevidenceList.map((option) => (
+             <option key={option._id} value={option.Name}>
+             {option.Name}
+             </option>
+             ))}
              </select>            
             
-             <label className="label1">Select the Game,when done then click "Add Ghost"</label>
+             <label className="label1">Select the Game</label>
              <select className="dropdown" required >
              <option value="">Select evidence</option>
              <option value="evidence1">Evidence 1</option>
